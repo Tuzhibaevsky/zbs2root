c258b811b6e23f5b3f8d17f8a1124f4a027f6aaa
Yikai Xu <xyk21@mails.tsinghua.edu.cn>
2025-02-09 22:16:20 +0900

    Sync from Cedar

--- GNUmakefile
+++ GNUmakefile
@@ -4,6 +4,10 @@ endif
 
 include $(SKOFL_ROOT)/config.gmk
 
+read_zbs: read_zbs.o
+	LD_RUN_PATH=$(LIBDIR):$(ATMPD_SRC)/include/:$(SKOFL_LIBDIR):$(LOCAL_INC) $(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
+
+
 clean: 
 	$(RM) *.o *~ core fort.* *.root *.zbs *.log 
 	$(RM) read_zbs
--- read_zbs.cc
+++ read_zbs.cc
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 
 // SK related
 
@@ -13,8 +14,19 @@
 #undef MAXHWSK
 #include <skheadC.h>
 #include <sktqC.h>
-
 #include <geopmtC.h>
+#include <vcworkC.h>
+
+#include "/usr/local/sklib_gcc8/atmpd_23c/src/recon/ska/zbsmc.h"
+
+#define MAX_MC_VERTICES           10
+#define MAX_MC_PRIMARIES          50
+#define MAX_MC_SECONDARIES        400
+#define MAX_GENERATOR_PRIMARIES   100
+
+#define ZBSMC_NO_BANK  -1
+#define ZBSMC_TOO_MANY -2
+
 
 extern "C" {
   void geoset_();
@@ -23,6 +35,8 @@ extern "C" {
   int  skread_(int*);
   void skroot_init_(int*);
   void kzinit_();
+  void vcrdvccm_();
+  //void skgetv_();
   void set_rflist_(int*, const char*, const char*, const char*, const char*,
 		   const char*, const char*, const char*, const char*, const char*,
 		   int, int, int, int, int, int, int, int, int);
@@ -36,10 +50,21 @@ extern "C" {
 int main(int argc, char *argv[])
 {
   if(argc < 3) {
-    printf("Usage: sample input.root output.root\n");
+    printf("Usage: read_zbs input.zbs output.root\n");
     exit(0);
   }
 
+  /*
+  struct convvect_emb convvect_emb;
+  struct convvect     convvect[MAX_MC_SECONDARIES];
+
+    if (get_convvect(&convvect_emb, convvect, MAX_MC_SECONDARIES) !=
+      ZBSMC_NO_BANK)
+    {
+    	std::cout << "CONVVECT NP: " << convvect_emb.n_particles << std::endl;
+    }
+
+  */
   kzinit_();
 
   // open skroot file
@@ -67,18 +92,105 @@ int main(int argc, char *argv[])
   TFile OutF(argv[2], "RECREATE");
   TTree OutT("T","T");
   std::vector<Int_t> I; std::vector<Double_t> T, Q, X, Y, Z;
+  int    nvc;
+  float  posvc[3];
+  int    ipvc[100];
+  float  amasvc[100];
+  float  pvc[100][3];
+  int    iorgvc[100];
+  int    iflgvc[100];
+  int    icrnvc[100];
+  float  timvc[100];
+  float  posivc[100][3];
+  int    ivtivc[100];
+  float  posfvc[100][3];
+  int    ivtfvc[100];
+  /**/
+/*     NVC         : # OF PARTICLE*/
+/*     POSVC(3)    : VERTEX POSITION OF INITIAL INTERACTION*/
+/*     IPVC(I)     : PARTICLE CODE OF I-TH PARTICLE*/
+/*     AMASVC(I)   : MASS OF I-TH PARTICLE ( MEV/C**2 )*/
+/*     PVC(3,I)    : MOMENTUM OF I-TH PARTICLE ( MEV/C )*/
+/*     IORGVC(I)   : ID OF ORIGIN PARTICLE*/
+/*     IFLGVC(I)   : FLAG OF FINAL STATE */
+/*                   0 : DETERMINED LATER PROCEDURE*/
+/*                   1 : DECAY TO OTHER PARTICLE*/
+/*                   2 : ESCAPE FROM DETECTOR*/
+/*                   3 : ABSORPTION*/
+/*                   4 : CHARGE EXCHANGE*/
+/*                   5 : STOP AND NOT CONSIDER IN M.C. */
+/*                   6 : E.M. SHOWER*/
+/*                   7 : Particle production*/
+/*                   8 : Inelastic scattering*/
+/*     ICRNVC(I)   : FLAG OF TO CHASE OR NOT*/
+/*                   0 : DON'T CHASE*/
+/*                   1 : CHASE */
+/*     TIMVC(I)    : TIME OF VERTEX*/
+/*     POSIVC(3,I) : INITIAL VERTEX POSITION*/
+/*     IVTIVC(I)   : ID OF INITIAL VERTEX*/
+/*     POSFVC(3,I) : FINAL VERTEX POSITION*/
+/*     IVTFVC(I)   : ID OF FINAL VERTEX*/
+/**/
+/*common vcwork was here*/
+
   OutT.Branch("ID", &I);
   OutT.Branch("T", &T);
   OutT.Branch("Q", &Q);
   OutT.Branch("X", &X);
   OutT.Branch("Y", &Y);
   OutT.Branch("Z", &Z);
+  OutT.Branch("nvc", &nvc, "nvc/I");
+  OutT.Branch("posvc", &posvc, "posvc[3]/F");
+  OutT.Branch("posivc", &posivc, "posivc[nvc][3]/F");
+  OutT.Branch("posfvc", &posfvc, "posfvc[nvc][3]/F");
+  OutT.Branch("ipvc", &ipvc, "ipvc[nvc]/I");
+  OutT.Branch("amasvc", &amasvc, "amasvc[nvc][3]/F");
+  OutT.Branch("pvc", &pvc, "pvc[nvc][3]/F");
+  OutT.Branch("iorgvc", &iorgvc, "iorgvc[nvc]/I");
+  OutT.Branch("iflgvc", &iflgvc, "iflgvc[nvc]/I");
+  OutT.Branch("timvc", &timvc, "timvc[nvc]/F");
 
+  //  OutT.Branch("icrnvc", &icrnvc);
+  //  OutT.Branch("posivc", &posivc);
+  //  OutT.Branch("ivtivc", &ivtivc);
+  //  OutT.Branch("posfvc", &posfvc);
+  //  OutT.Branch("ivtfvc", &ivtfvc);
+
+  //int counter = 0;
+  //
+  
+  if (argc > 3){
+    std::cout << "Using 4th argument, creating geofile with name: " << argv[3] << std::endl;
+
+    char *filename = argv[3];
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL)
+      {
+        printf("Error opening the file %s", filename);
+        return -1;
+      }
+
+    for (int k=1; k<11147; k++){
+      float geofile_x = geopmt_.xyzpm[k-1][0];
+      float geofile_y = geopmt_.xyzpm[k-1][1];
+      float geofile_z = geopmt_.xyzpm[k-1][2];
+      float geofile_dx = geopmt_.dxyzpm[k-1][0];
+      float geofile_dy = geopmt_.dxyzpm[k-1][1];
+      float geofile_dz = geopmt_.dxyzpm[k-1][2];
+      //fprintf(fp, "%f,%f,%f,%f,%f,%f\n", geofile_x, geofile_y, geofile_z, geofile_dx, geofile_dy, geofile_dz);
+      //std::cout << "ID: " << k << ", dx: " << geofile_dx << ", dy: " << geofile_dy << ", dz: " << geofile_dz << std::endl;
+    }
+    fclose(fp);
+    return 0;
+  }
 
   while (!bEOF) {
-
+    //    if (counter>0)
+    //      break;
+    //    counter++;
     readStatus = skread_(&lun);
-
+    vcrdvccm_();
+    //skgetv_();
     switch (readStatus) {
     case 0: // event read                                               
       {
@@ -90,9 +202,12 @@ int main(int argc, char *argv[])
 	std::cout << " NQISK  " << skq_.nqisk
 		  << " QISMSK " << skq_.qismsk
 		  << std::endl;
-
+	std::cout << " Incident particle ID " << vcwork_.ipvc[0]
+		  << ". Total number of particles " << vcwork_.nvc
+		  << std::endl;
+	
 	const auto NQISK = skq_.nqisk;
-
+	
 	I.clear();
 	T.clear();
 	Q.clear();
@@ -108,6 +223,28 @@ int main(int argc, char *argv[])
 	  X.emplace_back(geopmt_.xyzpm[icab-1][0]);
 	  Y.emplace_back(geopmt_.xyzpm[icab-1][1]);
 	  Z.emplace_back(geopmt_.xyzpm[icab-1][2]);
+	}
+
+	nvc = vcwork_.nvc;
+	posvc[0] = vcwork_.posvc[0];
+	posvc[1] = vcwork_.posvc[1];
+	posvc[2] = vcwork_.posvc[2];
+	for (int i=0; i<nvc; ++i) {
+	  ipvc[i] = vcwork_.ipvc[i];
+	  amasvc[i] = vcwork_.amasvc[i];
+	  timvc[i] = vcwork_.timvc[i];
+	  pvc[i][0] = vcwork_.pvc[i][0];
+	  pvc[i][1] = vcwork_.pvc[i][1];
+	  pvc[i][2] = vcwork_.pvc[i][2];
+	  posivc[i][0] = vcwork_.posivc[i][0];
+	  posivc[i][1] = vcwork_.posivc[i][1];
+	  posivc[i][2] = vcwork_.posivc[i][2];
+	  posfvc[i][0] = vcwork_.posfvc[i][0];
+	  posfvc[i][1] = vcwork_.posfvc[i][1];
+	  posfvc[i][2] = vcwork_.posfvc[i][2];
+	  std::cout << "time: " << vcwork_.timvc[i] << std::endl;
+	  iorgvc[i] = vcwork_.iorgvc[i];
+	  iflgvc[i] = vcwork_.iflgvc[i];	  
 	}
 
 	OutT.Fill();
